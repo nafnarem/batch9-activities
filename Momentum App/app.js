758e3dc65c7a87f31812120a94b7ce8c79be84fd
John Parreno <johnparreno@gmail.com>
2021-05-18 21:08:19 +0800

    Functionality Done

--- Momentum App/app.js
+++ Momentum App/app.js
@@ -1,28 +1,98 @@
-const clockContainer = document.querySelector(".clock")
-const clockTitle = clockContainer.querySelector("h1")
+//todolist
+const LOCAL_STORAGE_USER_KEY = 'user'
+let displayName = localStorage.getItem(LOCAL_STORAGE_USER_KEY)
+const quotesDisplay = document.querySelector('.modal-body');
+const userForm = document.querySelector('[data-user-form]');
+const userInput = document.querySelector('[data-user-input]');
+let userName = document.querySelector('[data-name]');
+let btn = document.getElementById('quoteButton');
+let output = document.getElementById('output');
+let btnQuote= document.getElementById('quoteSubmit')
+const newQuote = document.querySelector('[data-user-quote]')
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-function getTime(){
-	let date = new Date();
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
-	let seconds = date.getSeconds();
+if(displayName){
+  userName.innerHTML=displayName;
+}
+//Display User
 
-    if (hours   < 10) {
-        hours   = "0"+date.getHours();
-    }
-    if (minutes < 10) {
-        minutes = "0"+date.getMinutes();
-    }
-    if (seconds < 10) {
-        seconds = "0"+date.getSeconds();
-    }
+  
+  userForm.addEventListener('submit', e => { 
+  e.preventDefault()
+  localStorage.setItem(LOCAL_STORAGE_USER_KEY, userInput.value);
+  userName.innerHTML = localStorage.getItem(LOCAL_STORAGE_USER_KEY);
+  closeModal(modal)
+})
 
-	clockTitle.innerText = hours + ':' +minutes+ ':' + seconds;
+//Modal
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+  quotesDisplay.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
 }
 
-function init(){
-	getTime();
-	setInterval(getTime, 1000); // Not getTime()
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
 
-init();
+//quotes
+var quotes = [
+  "Stay Hungry. Stay Foolish. - Steve Jobs",
+  "Good Artists Copy, Great Artists Steal.- Pablo Picasso",
+  "Argue with idiots, and you become an idiot.- Paul Graham",
+  "Be yourself; everyone else is already taken. - Oscar Wilde",
+  "Simplicity is the ultimate sophistication. - Leonardo Da Vinci",
+  "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty. – Winston Churchill",
+  "Don’t Let Yesterday Take Up Too Much Of Today. – Will Rogers",
+  "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.” – Unknown",
+  "It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.” – Inspirational Quote By Vince Lombardi",
+  "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.” – Steve Jobs",
+  "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.” – Rob Siltanen",
+  "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.” – Og Mandino",
+  "Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.” – Mohnish Pabrai",
+  "We May Encounter Many Defeats But We Must Not Be Defeated.” – Maya Angelou",
+  "Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do. – Johann Wolfgang Von Goethe"
+]
+
+btn.addEventListener('click', function(){
+  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  output.innerHTML = randomQuote;
+})
+
+
+btnQuote.addEventListener('click', function(){
+  if(newQuote.value.length>5){
+    quotes.push(newQuote.value);
+    output.innerHTML= quotes[quotes.length-1];
+    newQuote.value= "";
+  }
+  this.preventDefault;
+})
+
---
+++ Momentum App/clock.js
@@ -0,0 +1,47 @@
+const clockContainer = document.querySelector(".clock")
+const clockTitle = clockContainer.querySelector("h1")
+const calendarTitle = clockContainer.querySelector("h2")
+
+function getTime(){
+	let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+    let weekday = new Array(7);
+        weekday[0] = "Sun";
+        weekday[1] = "Mon";
+        weekday[2] = "Tues";
+        weekday[3] = "Wed";
+        weekday[4] = "Thurs";
+        weekday[5] = "Fri";
+        weekday[6] = "Sat";
+    let now = weekday[date.getDay()];
+
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    if (hours   < 10) {
+        hours   = "0"+date.getHours();
+    }
+    if (minutes < 10) {
+        minutes = "0"+date.getMinutes();
+    }
+    if (seconds < 10) {
+        seconds = "0"+date.getSeconds();
+    }
+    if (hours >12){
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+    }
+    calendarTitle.innerText = now + " "+  (month+1) + "/" +day +"/"+ year;
+    clockTitle.innerText = hours + ':' +minutes+ ':' + seconds + ''+ ampm;
+}
+
+
+function init(){
+	getTime();
+	setInterval(getTime, 1000); // Not getTime()
+}
+
+init();
